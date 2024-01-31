70fa9155c05f998d7cd63cd2c90f7e865a715910
Dani$ <152904949+Addanyass@users.noreply.github.com>
2024-01-31 20:49:19 +0300

    Add files via upload

---
+++ practick1.js
@@ -0,0 +1,11 @@
+let age = 29;
+
+if (age > 30){
+    console.log("вам больше 30 лет");
+}
+else if (age ==30){
+    console.log("вам ровно 30 лет");
+}
+else {
+    console.log("вам меньше 30 лет");
+}
